9e6cb8f9f18995dd97719d841d72930b1f0a1404
rvickram <rvickram@uwo.ca>
2018-02-17 21:05:29 -0500

    Initial commit

---
+++ src/assignment1/Assignment1.java
@@ -0,0 +1,43 @@
+/*
+By: Ryan Vickramasinghe
+ */
+package assignment1;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ryanv
+ */
+public class Assignment1 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {   //main form, loads start window
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.getIcons().add(new Image("file:src/assignment1/WesternLogo.png"));
+        stage.setTitle("Assignment 1");
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/assignment1/MainController.java
@@ -0,0 +1,22 @@
+package assignment1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author ryanv
+ */
+public class MainController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
