f1396ca786a0b401d741686908c35ef3af5e1b27
rvickram <rvickram@uwo.ca>
2018-02-24 20:53:10 -0500

    Array Visualization Complete
    Program is able to draw rectangles based upon an array of random heights.
    
    Co-Authored-By: mpitassi <mpitassi@users.noreply.github.com>

--- src/assignment1/MainController.java
+++ src/assignment1/MainController.java
@@ -4,12 +4,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import java.util.concurrent.ThreadLocalRandom;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ComboBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 /**
  * FXML Controller class, controls Main
@@ -21,6 +25,8 @@ public class MainController implements Initializable {
     ObservableList<String> listItems = FXCollections.observableArrayList();
     
     @FXML
+    AnchorPane recDisplay;
+    @FXML
     Label labelSize;
     @FXML
     Slider sliderArraySize;
@@ -30,14 +36,56 @@ public class MainController implements Initializable {
     ComboBox comboAlgorithms;
     
     @FXML
-    public void arraySizeBar_ValueChanged(){ //changes the label to slider value
+    public void arraySizeBar_ValueChanged(){ //updates array and slider label
+        //update the label:
         labelSize.setText(Long.toString(Math.round(sliderArraySize.getValue())));
+        
+        //update the array:
+        //INSERT SETARRAYSIZE
+        //CALL DRAWFUNCTION
+    }
+    
+    @FXML
+    public void sortBtn_Click(){
+        drawRectangles();
     }
     
     @FXML
     public void resetBtn_Click(){ //resets program to initial state
         sliderArraySize.setValue(1);
         labelSize.setText("1");
+    }
+    
+    private void drawRectangles(){
+        //clear pane:
+        recDisplay.getChildren().clear();
+        
+        //temporary array filled with random ints
+        int[] array = new int[(int)sliderArraySize.getValue()];
+        for(int i = 0; i < (int)sliderArraySize.getValue(); i++){
+            //loads array with random integers
+            array[i] = ThreadLocalRandom.current().nextInt(1, recDisplay.heightProperty().getValue().intValue());
+            
+            /*
+            //draws rectangle based on height:
+            double width = (recDisplay.widthProperty().getValue()/sliderArraySize.getValue());
+            double height = (array[i]/sliderArraySize.getValue())*recDisplay.heightProperty().getValue();
+            */
+            double width = (recDisplay.widthProperty().getValue()/sliderArraySize.getValue());
+            double height = (array[i]);
+            
+            double x = width*i;
+            double y = recDisplay.heightProperty().getValue() - height;
+            
+            Rectangle rec = new Rectangle();
+            rec.setX(x);
+            rec.setY(y);
+            rec.setWidth(width);
+            rec.setHeight(height);
+            rec.setFill(Color.RED);
+            
+            recDisplay.getChildren().add(rec);
+        }
     }
     
     private void setSortStrategy(){
